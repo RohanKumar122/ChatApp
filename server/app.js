eb8c477a3b868e958bf62946a64248d303dcaf9f
Rohan Kumar <rk399504@gmail.com>
2025-01-17 12:49:29 +0530

    Update server user

--- server/app.js
+++ server/app.js
@@ -2,24 +2,26 @@ import express from "express";
 import userRoute from "./routes/user.js";
 import { connnectDB } from "./utils/features.js";
 import dotenv from "dotenv";
+import { errorMiddleware } from "./middlewares/error.js";
 dotenv.config();
 
-const MONGO_URI = process.env.MONGO_URI 
-const PORT = process.env.PORT || 5000
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
 connnectDB(MONGO_URI);
 
 const app = express();
 
 app.use(express.json());
 
-const mainPage = (req, res) => {
-    res.send("Hello World");
-};
 
-app.post("/", mainPage);
 app.use("/user", userRoute);
 
+app.post("/", (req, res) => {
+    res.send("Hello World");
+  });
+
+app.use(errorMiddleware)  
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,32 +1,42 @@
+import { TryCatch } from "../middlewares/error.js";
 import { User } from "../models/user.js";
 import { sendToken } from "../utils/features.js";
+import { compare } from "bcrypt";
 
 const newUser = async (req, res) => {
-  const {name,username,password,bio}=req.body;
+  const { name, username, password, bio } = req.body;
   const avatar = {
     public_id: "dnqd1",
     url: "dwsdwd1",
   };
-  
 
-  console.log(req.body, name, username,password);
+  console.log(req.body, name, username, password);
 
   const user = await User.create({
     name,
     bio,
     username,
     password,
-    avatar
+    avatar,
   });
   console.log(req.body);
-  
+
   sendToken(res, user, 201, "User created successfully");
-
 };
 
+const login =TryCatch( async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username }).select("+password");
 
-const login = async (req, res) => {
-  res.send("Login");
-};
+  if (!user) return next(new Error("Invalid Username"));
 
-export {login, newUser};
+  const isPasswordMatched = await compare(password, user.password);
+
+  if (!isPasswordMatched) return next(new Error("Invalid Password"));
+
+  sendToken(res, user, 200, `Welcome Back ${user.name}`)
+})
+
+
+
+export { login, newUser };
